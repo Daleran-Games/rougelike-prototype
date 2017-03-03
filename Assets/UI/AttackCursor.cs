6ea946f2889bd8ea52a963214bd3b17d90d1b048
daleran <daleran@gmail.com>
2017-03-03 12:11:11 -0600

    COmbined cursor classes and fixed map path bug

--- Assets/UI/AttackCursor.cs
+++ Assets/UI/AttackCursor.cs
@@ -11,6 +11,9 @@ namespace Dallib.CameraTools
         Color32 defaultColor;
         [SerializeField]
         Color32 attackColor;
+        [SerializeField]
+        [Range(0f,1f)]
+        float distanceScaler = 1f;
 
         SpriteRenderer cursorRenderer;
         GameObject player;
@@ -25,14 +28,27 @@ namespace Dallib.CameraTools
         void Start()
         {
             player = GameObject.FindGameObjectWithTag("Player");
+            Cursor.visible = false;
+            cursorRenderer.color = defaultColor;
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 newPos = Vector3.MoveTowards(player.transform.position, mousePos, Vector3.Distance(player.transform.position, mousePos) * 0.5f);
-            transform.position = new Vector3(newPos.x, newPos.y, 0f);
+
+            if (distanceScaler < 1)
+            {
+                Vector3 newPos = Vector3.MoveTowards(player.transform.position, mousePos, Vector3.Distance(player.transform.position, mousePos) * distanceScaler);
+                transform.position = new Vector3(newPos.x, newPos.y, 0f);
+            }
+            else
+                transform.position = new Vector3(mousePos.x, mousePos.y, 0f);
+
+            if (GameManager.Instance.Play.PlayerHighlightingTarget == true)
+                cursorRenderer.color = attackColor;
+            else
+                cursorRenderer.color = defaultColor;
         }
     }
 }
